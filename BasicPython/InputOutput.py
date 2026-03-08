fdbec2aa4761b753387c1269a35a6e3f87ccc1f2
RohanMagar7 <magarohan8@gmail.com>
2026-03-08 18:59:23 +0530

    Input and Output

---
+++ BasicPython/InputOutput.py
@@ -0,0 +1,13 @@
+print("Rohan Magar") # print() funtion use to print anything in terminal 
+
+a = 1
+print("A store ",a) #here we print string and argrument a
+
+# for the input() function use to get any input 
+
+a = input("Enter Number : ") # Enter Number : 23
+
+# when enter 23 that store in variable a = 23 
+
+b = int(input("Enter Number : ")) # now we Enter only number string 
+
