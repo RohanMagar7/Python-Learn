982907855b62c9a30123075d4e10310edbea14d9
RohanMagar7 <magarohan8@gmail.com>
2026-03-08 22:47:54 +0530

    learn operations in python

---
+++ BasicPython/Operator.py
@@ -0,0 +1,50 @@
+# Operators 
+
+# Arithmetic, Assignment, comparison, logical operations, and more.
+
+# Arithmetic operations 
+
+print(1+2) # 3
+print(1-2) # -1
+print(2*3) # 6
+print(2/2) # 1.0
+print(2//2)# 1.0
+print(3%2) # 0
+print(2**2)# 4 
+
+#Assignment operations
+
+a = 23
+
+# compound assignmet opertions 
+print("comount assignment")
+a += 12
+a -= 21
+a *= 12
+a /= 21
+print(a) # 8.0
+
+
+# comparision operations(6)
+# comparision operations always provide result in booleans
+print("comparision")
+a = 3 
+b = 2
+print(a==b) #False 
+print(a!=b) #True
+print(a<b)  #False
+print(a>b)  #True
+print(a>=b) #True
+print(a<=b) #False 
+
+# String are compara with asci code we can not compare string to number 
+
+# Logical Operators () 
+# There Three Operator in logical operators (or and not)
+print("logical operations")
+# AND operation Are need all true for true 
+print(56 < 100 and 23==23 and 12 < 23) # True 
+# OR operation are need one is true 
+print(123 < 100 or 23 ==23 or 34 < 23) #  
+
+
