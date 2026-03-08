b0bfd29c29cceb8fdb2eb54bab94c3ecf03ae554
RohanMagar7 <magarohan8@gmail.com>
2026-03-08 23:31:15 +0530

    learn Conditional Statement

---
+++ ConditionalStatements/ifelse.py
@@ -0,0 +1,26 @@
+# IF ELSE
+
+# Generally there are 3 types of variation in conditional statements
+# if 
+# if else
+# if-elif-else
+
+# 1 IF 
+a = 12
+if(a < 18): # this True then run if 
+    print("Your not")
+else: # this if are false then run this 
+    print("Your yes")
+
+money = int(input("please provide me the money : "))
+
+if money == 10:
+    print("I will have a choco ber icecream")
+elif money == 20:
+    print("I will have a mongo dolly")
+else:
+    print("I will have a cone")
+
+
+
+    
---
+++ ConditionalStatements/problem.py
@@ -0,0 +1,33 @@
+# Accept two Numbers and print the greatest between them.
+
+# a = int(input("provide A : "))
+# b = int(input("provide B : "))
+
+# if a > b:
+#     print("A is Greater")
+# elif b > a:
+#     print("B is Greater")
+# else:
+#     print("Both Equal")
+
+# Accept the gender from the user as char and print the respactive greeting message.
+
+# gender = str(input("Provide Gender ( F ) ( M ) :"))
+
+# if('F' == gender or 'f' == gender):
+#     print("Good Morning Ma'am")
+# elif('M' == gender or 'm' == gender):
+#     print("Good Morning Sir")
+# else:
+#     print("Wrong Input")
+
+
+# check give number is evan or not 
+
+num = int(input("provide Number : "))
+
+if num%2==0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
+
