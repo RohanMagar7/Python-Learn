281b298262c1afb93bb377873b83535cbabff6fb
RohanMagar7 <magarohan8@gmail.com>
2026-03-08 16:46:39 +0530

    learn Data Type Numbers

---
+++ BasicPython/DataType.py
@@ -0,0 +1,24 @@
+''' - Data types are the things we store in variables and it 
+      defines what data type variabels are
+    - Python has built in data types for different kinds of data
+'''
+
+# Basic Data Type 
+
+''' - Numbers 
+   Integer - all the numbers excluding decimal places and fraction.
+   Float - All the decimal numbers and fraction values are float.
+   Complex - Numbers with real and imaginary parts are complex 
+'''
+
+a = 12 # we can write -12 thsi is also integar 
+print(type(a))
+# <class 'int'>
+
+b = 34.3 # we can write also 12/3 that also float data type (fraction)
+print(type(b))
+# <class 'float'>
+
+c = 34j # j use for imaginary values you can only use j not other r,s,t,e
+print(type(c))
+# <class 'complex'>
